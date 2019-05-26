863aa7609995f8feae61c6416780d054bc2ef543
johnmmm <zhoujh15@mails.tsinghua.edu.cn>
2019-05-26 11:48:07 +0800

    copy gptree from the Int, and mysterious make

--- src/GPTree.h
+++ src/GPTree.h
@@ -1,5 +1,5 @@
-#ifndef GPTree_h
-#define GPTree_h
+#ifndef GPTREE_H
+#define GPTREE_H
 
 #include <time.h>
 #include <stdio.h>
@@ -12,6 +12,18 @@ class GPTree
 public:
     GPTree();
     ~GPTree();
+
+	void init();
+	void read();
+	void save();
+	void load();
+	void build_tree();
+
+	int get_min_distance(int S, int T);
+
+	//void test();
+	void read_node();
+	void output();
     
     void test_cmake();
 };
